ae177c5264885f76fabfd5d65144469a2df69fed
JessicaLassie <jessica.lassie@gmail.com>
2020-08-10 22:44:43 +0200

    [Add] main class

---
+++ src/fr/jl/encryption/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) Jessica LASSIE from 2020 to present
+ * All rights reserved
+ */
+package fr.jl.encryption.main;
+
+import fr.jl.encryption.view.JfEncryption;
+
+/**
+ *
+ * @author Jessica LASSIE
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        new JfEncryption();
+    }
+    
+}
